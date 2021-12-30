23acdd9c7a214b25fd40427fd02fea9fa690d8eb
gustavo <gustavo.isaac@gmail.com>
2021-12-30 15:50:07 +0000

    InstagramProfile - ProfileSection

--- app/src/main/java/com/artemissoftware/jetpackcomposetutorial4/ui/instagramprofile/InstagramProfileScreen.kt
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial4/ui/instagramprofile/InstagramProfileScreen.kt
@@ -1,13 +1,12 @@
 package com.artemissoftware.jetpackcomposetutorial4.ui.instagramprofile
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.artemissoftware.jetpackcomposetutorial4.ui.instagramprofile.composables.ProfileSection
 import com.artemissoftware.jetpackcomposetutorial4.ui.instagramprofile.composables.TopBar
 import com.artemissoftware.jetpackcomposetutorial4.ui.jetmeditation.MeditationScreen
 
@@ -21,6 +20,11 @@ fun InstagramProfileScreen(){
             modifier = Modifier
                 .padding(10.dp)
         )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        ProfileSection()
+
     }
 
 }
---
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial4/ui/instagramprofile/composables/ProfileSection.kt
@@ -0,0 +1,190 @@
+package com.artemissoftware.jetpackcomposetutorial4.ui.instagramprofile.composables
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.artemissoftware.jetpackcomposetutorial4.R
+
+@Composable
+fun ProfileSection(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            RoundImage(
+                image = painterResource(id = R.drawable.artemis_3),
+                modifier = Modifier
+                    .size(100.dp)
+                    .weight(3f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            StatSection(modifier = Modifier.weight(7f))
+        }
+        ProfileDescription(
+            displayName = "Ancient Greek deity",
+            description = "Artemis was one of the most widely venerated of the Ancient Greek deities, and her temple at Ephesus was one of the Seven Wonders of the Ancient World. Artemis' symbols included a bow and arrow, a quiver, and hunting knives, and the deer and the cypress were sacred to her",
+            url = "https://en.wikipedia.org/wiki/Artemis",
+            followedBy = listOf("Aphrodite", "Diana"),
+            otherCount = 17
+        )
+    }
+}
+
+@Composable
+fun RoundImage(
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape)
+    )
+}
+
+
+
+@Composable
+fun StatSection(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+    ) {
+        ProfileStat(numberText = "601", text = "Posts")
+        ProfileStat(numberText = "100K", text = "Followers")
+        ProfileStat(numberText = "72", text = "Following")
+    }
+}
+
+@Composable
+fun ProfileStat(
+    numberText: String,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = numberText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = text)
+    }
+}
+
+
+
+@Composable
+fun ProfileDescription(
+    displayName: String,
+    description: String,
+    url: String,
+    followedBy: List<String>,
+    otherCount: Int
+) {
+    val letterSpacing = 0.5.sp
+    val lineHeight = 20.sp
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = displayName,
+            fontWeight = FontWeight.Bold,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            text = description,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Text(
+            text = url,
+            color = Color(0xFF3D3D91),
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        if(followedBy.isNotEmpty()) {
+            Text(
+                text = buildAnnotatedString {
+                    val boldStyle = SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold
+                    )
+                    append("Followed by ")
+                    followedBy.forEachIndexed { index, name ->
+                        pushStyle(boldStyle)
+                        append(name)
+                        pop() //remove style
+                        if(index < followedBy.size - 1) {
+                            append(", ")
+                        }
+                    }
+                    if(otherCount > 2) {
+                        append(" and ")
+                        pushStyle(boldStyle)
+                        append("$otherCount others")
+                    }
+                },
+                letterSpacing = letterSpacing,
+                lineHeight = lineHeight
+            )
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+private fun RoundImagePreview() {
+    RoundImage(
+        image = painterResource(id = R.drawable.artemis_3),
+        modifier = Modifier
+            .size(100.dp)
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun DefaultPreview() {
+    ProfileSection()
+}
