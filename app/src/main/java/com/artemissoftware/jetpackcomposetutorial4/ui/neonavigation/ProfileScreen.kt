4394bbccbd7eb5666f0b2ac946a65fd0e90d99aa
gustavo <gustavo.isaac@gmail.com>
2022-01-19 16:09:43 +0000

    NeoNavigation - ProfileScreen

---
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial4/ui/neonavigation/ProfileScreen.kt
@@ -0,0 +1,51 @@
+package com.artemissoftware.jetpackcomposetutorial4.ui.neonavigation
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.navigation.NavController
+import com.artemissoftware.jetpackcomposetutorial4.ui.neonavigation.models.User
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ProfileScreen(
+    navController: NavController,
+    name: String,
+    userId: String,
+    created: Long
+) {
+    val user = remember {
+        User(
+            name = name,
+            id = userId,
+            created = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(created), ZoneId.systemDefault()
+            )
+        )
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Profile Screen: $user", textAlign = TextAlign.Center)
+        Button(onClick = {
+            navController.navigate("post/true")
+        }) {
+            Text("Go to Post Screen")
+        }
+    }
+}
